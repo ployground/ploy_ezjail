3dfa3891f0b5d97e9ca7c2001a2d1f7dc0c51172
Florian Schulze <florian.schulze@gmx.net>
2014-06-16 19:17:48 +0200

    Moved ProxyInstance to mr.awsome.

--- setup.py
+++ setup.py
@@ -23,7 +23,7 @@ setup(
     namespace_packages=['mr'],
     install_requires=[
         'setuptools',
-        'mr.awsome >= 1.0rc2',
+        'mr.awsome >= 1.0rc8',
         'lazy'],
     entry_points="""
         [mr.awsome.plugins]
