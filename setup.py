4c9386b5fe0e407bf5eb85bc4aed5d47987c3342
Florian Schulze <florian.schulze@gmx.net>
2014-07-02 14:01:37 +0200

    Add Python 3 compatibility, because ... why not!

--- setup.py
+++ setup.py
@@ -22,7 +22,7 @@ setup(
     packages=['ploy_ezjail'],
     install_requires=[
         'setuptools',
-        'ploy >= 1.0rc9',
+        'ploy >= 1.0rc10',
         'lazy'],
     entry_points="""
         [ploy.plugins]
