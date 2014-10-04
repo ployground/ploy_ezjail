94d5b0f08d05b67c0ab206603e5f4a2f8a911996
Florian Schulze <florian.schulze@gmx.net>
2014-10-04 08:01:16 +0200

    Bump requirement to release.

--- setup.py
+++ setup.py
@@ -31,7 +31,7 @@ setup(
     packages=['ploy_ezjail'],
     install_requires=[
         'setuptools',
-        'ploy > 1.0.1',
+        'ploy >= 1.0.2',
         'lazy'],
     entry_points="""
         [ploy.plugins]
