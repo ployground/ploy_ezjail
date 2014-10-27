fc5e8ad747fad6ddb5d5874abbe2aa0f44adc3f8
Florian Schulze <florian.schulze@gmx.net>
2014-10-27 10:19:42 +0100

    There are plans for ploy 2, with a different API.

--- setup.py
+++ setup.py
@@ -31,7 +31,7 @@ setup(
     packages=['ploy_ezjail'],
     install_requires=[
         'setuptools',
-        'ploy >= 1.0.2',
+        'ploy >= 1.0.2, < 2dev',
         'lazy'],
     entry_points="""
         [ploy.plugins]
